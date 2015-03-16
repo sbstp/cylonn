a10dde8b91d06f2884e1038e4ce5870285315b1a
Alexandre Gagnon <alxgnon@gmail.com>
2015-03-16 12:57:13 -0400

    init: Add line number printing for syntax errors

--- src/init.rs
+++ src/init.rs
@@ -5,19 +5,31 @@ use std::old_io::{BufferedReader, File, IoError, IoResult};
 use plugin::Plugin;
 
 #[derive(Copy, PartialEq, Eq, Debug)]
-pub enum SyntaxError {
+pub enum SyntaxErrorKind {
     NoColon,
     NoName,
     NoCommand,
 }
 
-impl fmt::Display for SyntaxError {
+impl fmt::Display for SyntaxErrorKind {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            SyntaxError::NoColon => write!(f, "{}", "No colon delimiter found"),
-            SyntaxError::NoName => write!(f, "{}", "Plugin has no name"),
-            SyntaxError::NoCommand => write!(f, "{}", "Plugin has no command"),
+            SyntaxErrorKind::NoColon => write!(f, "{}", "No colon delimiter found"),
+            SyntaxErrorKind::NoName => write!(f, "{}", "Plugin has no name"),
+            SyntaxErrorKind::NoCommand => write!(f, "{}", "Plugin has no command"),
         }
+    }
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct SyntaxError {
+    pub kind: SyntaxErrorKind,
+    pub line_no: u32,
+}
+
+impl fmt::Display for SyntaxError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} on line {}", self.kind, self.line_no)
     }
 }
 
@@ -31,7 +43,7 @@ impl fmt::Display for ReadError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
             ReadError::IoError(ref err) => write!(f, "I/O Error: {}", err),
-            ReadError::SyntaxError(err) => write!(f, "Syntax Error: {}", err),
+            ReadError::SyntaxError(ref err) => write!(f, "Syntax Error: {}", err),
         }
     }
 }
@@ -48,20 +60,23 @@ impl error::FromError<SyntaxError> for ReadError {
     }
 }
 
-fn parse_line<'a>(line: &'a str) -> Result<Option<Plugin>, SyntaxError> {
+fn parse_line<'a>(line_no: u32, line: &'a str) -> Result<Option<Plugin>, SyntaxError> {
     let ln = line.trim();
     if ln.is_empty() || ln.starts_with('#') {
         Ok(None)
     } else {
         match ln.find(':') {
-            None => Err(SyntaxError::NoColon),
+            None => Err(SyntaxError{kind: SyntaxErrorKind::NoColon,
+                                    line_no: line_no}),
             Some(i) => {
                 let name = ln[..i].trim();
                 let cmd = ln[i+1..].trim();
                 if name.is_empty() {
-                    Err(SyntaxError::NoName)
+                    Err(SyntaxError{kind: SyntaxErrorKind::NoName,
+                                    line_no: line_no})
                 } else if cmd.is_empty() {
-                    Err(SyntaxError::NoCommand)
+                    Err(SyntaxError{kind: SyntaxErrorKind::NoCommand,
+                                    line_no: line_no})
                 } else {
                     Ok(Some(Plugin::new(name, cmd)))
                 }
@@ -74,8 +89,10 @@ pub fn read_init(path: &Path) -> Result<Vec<Plugin>, ReadError> {
     let mut plugins = Vec::new();
     let file = try!(File::open(path));
     let mut reader = BufferedReader::new(file);
+    let mut line_no = 0;
     while let Ok(ref line) = reader.read_line() {
-        if let Some(plugin) = try!(parse_line(line)) {
+        line_no += 1;
+        if let Some(plugin) = try!(parse_line(line_no, line)) {
             plugins.push(plugin);
         }
     }
@@ -84,71 +101,71 @@ pub fn read_init(path: &Path) -> Result<Vec<Plugin>, ReadError> {
 
 #[test]
 fn test_parse_line() {
-    let plugin = parse_line("test: run -a script").unwrap().unwrap();
+    let plugin = parse_line(1, "test: run -a script").unwrap().unwrap();
     assert_eq!(plugin.name, "test");
     assert_eq!(plugin.cmd, "run -a script");
 }
 
 #[test]
 fn test_parse_line_spacey() {
-    let plugin = parse_line("  test :  run -a script  ").unwrap().unwrap();
+    let plugin = parse_line(1, "  test :  run -a script  ").unwrap().unwrap();
     assert_eq!(plugin.name, "test");
     assert_eq!(plugin.cmd, "run -a script");
 }
 
 #[test]
 fn test_parse_line_comment() {
-    assert!(parse_line("# don't run -a script").unwrap().is_none());
+    assert!(parse_line(1, "# don't run -a script").unwrap().is_none());
 }
 
 #[test]
 fn test_parse_line_comment_spacey() {
-    assert!(parse_line("  # don't run -a script  ").unwrap().is_none());
+    assert!(parse_line(1, "  # don't run -a script  ").unwrap().is_none());
 }
 
 #[test]
 fn test_parse_line_empty() {
-    assert!(parse_line("").unwrap().is_none());
+    assert!(parse_line(1, "").unwrap().is_none());
 }
 
 #[test]
 fn test_parse_line_empty_spacey() {
-    assert!(parse_line("    ").unwrap().is_none());
+    assert!(parse_line(1, "    ").unwrap().is_none());
 }
 
 #[test]
 fn test_parse_line_hash_name() {
-    let plugin = parse_line("hash#: this is valid").unwrap().unwrap();
+    let plugin = parse_line(1, "hash#: this is valid").unwrap().unwrap();
     assert_eq!(plugin.name, "hash#");
     assert_eq!(plugin.cmd, "this is valid");
 }
 
 #[test]
 fn test_parse_line_err_no_colon() {
-    assert_eq!(parse_line("I AM ERROR").unwrap_err(),
-               SyntaxError::NoColon);
+    assert_eq!(parse_line(1, "I AM ERROR").unwrap_err().kind,
+               SyntaxErrorKind::NoColon);
 }
 
 #[test]
 fn test_parse_line_err_no_name() {
-    assert_eq!(parse_line(": cat /dev/null").unwrap_err(),
-               SyntaxError::NoName);
+    assert_eq!(parse_line(1, ": cat /dev/null").unwrap_err().kind,
+               SyntaxErrorKind::NoName);
 }
 
 #[test]
 fn test_parse_line_err_no_name_spacey() {
-    assert_eq!(parse_line("  :  cat /dev/null   ").unwrap_err(),
-               SyntaxError::NoName);
+    assert_eq!(parse_line(1, "  :  cat /dev/null   ").unwrap_err().kind,
+               SyntaxErrorKind::NoName);
 }
 
 #[test]
 fn test_parse_line_no_cmd() {
-    assert_eq!(parse_line("nothing:").unwrap_err(),
-               SyntaxError::NoCommand);
+    assert_eq!(parse_line(1, "nothing:").unwrap_err().kind,
+               SyntaxErrorKind::NoCommand);
 }
 
 #[test]
 fn test_parse_line_no_cmd_spacey() {
-    assert_eq!(parse_line("   nothing:  ").unwrap_err(),
-               SyntaxError::NoCommand);
+    assert_eq!(parse_line(1, "   nothing:  ").unwrap_err().kind,
+               SyntaxErrorKind::NoCommand);
 }
