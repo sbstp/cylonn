c934778a7c741815ca25e901cf9b08b0079b66ec
Alexandre Gagnon <alxgnon@gmail.com>
2015-03-15 22:01:41 -0400

    plugin: improve parse_line's parsing

--- src/plugin.rs
+++ src/plugin.rs
@@ -1,8 +1,6 @@
 use std::old_io::{BufferedReader, Command, File, IoResult, Process};
 use std::old_io::process::StdioContainer;
 
-use regex::{Captures, Regex};
-
 pub struct Plugin {
     pub name: String,
     pub cmd: String,
@@ -48,17 +46,32 @@ impl Plugin {
     }
 }
 
-fn parse_line<'a>(line: &'a str) -> Option<(&'a str, &'a str)> {
-    static RE_LINE: Regex = regex!(r"^\s*([^#\s]+)\s*:\s*((\s*[^#\s]+)+)\s*$");
+#[derive(Debug, PartialEq)]
+enum Line<'a> {
+    Comment,
+    Plugin(&'a str, &'a str),
+    SyntaxError(&'static str),
+}
 
-    match RE_LINE.captures(line) {
-        Some(cap) => {
-            match (cap.at(1), cap.at(2)) {
-                (Some(name), Some(cmd)) => Some((name, cmd)),
-                _ => None,
-            }
-        },
-        _ => None,
+fn parse_line<'a>(line: &'a str) -> Line {
+    let ln = line.trim();
+    if ln.starts_with('#') {
+        Line::Comment
+    } else {
+        match ln.find(':') {
+            None => Line::SyntaxError("No colon delimiter found"),
+            Some(i) => {
+                let name = ln[..i].trim();
+                let cmd = ln[i+1..].trim();
+                if name.is_empty() {
+                    Line::SyntaxError("Plugin has no name")
+                } else if cmd.is_empty() {
+                    Line::SyntaxError("Plugin has no command")
+                } else {
+                    Line::Plugin(name, cmd)
+                }
+            },
+        }
     }
 }
 
@@ -67,7 +80,7 @@ pub fn read_init(path: &Path) -> IoResult<Vec<Plugin>> {
     let file = try!(File::open(path));
     let mut reader = BufferedReader::new(file);
     while let Ok(ref line) = reader.read_line() {
-        if let Some((name, cmd)) = parse_line(line) {
+        if let Line::Plugin(name, cmd) = parse_line(line) {
             plugins.push(Plugin::new(name, cmd));
         }
     }
@@ -77,17 +90,53 @@ pub fn read_init(path: &Path) -> IoResult<Vec<Plugin>> {
 #[test]
 fn test_parse_line() {
     assert_eq!(parse_line("test: run -a script"),
-               Some(("test", "run -a script")));
+               Line::Plugin("test", "run -a script"));
 }
 
 #[test]
-fn test_parse_line_trim() {
-    assert_eq!(parse_line("  test  :   run -a script  "),
-               Some(("test", "run -a script")));
+fn test_parse_line_spacey() {
+    assert_eq!(parse_line("  test :  run -a script  "),
+               Line::Plugin("test", "run -a script"));
 }
 
 #[test]
 fn test_parse_line_comment() {
     assert_eq!(parse_line("# don't run -a script"),
-               None);
+               Line::Comment);
+}
+
+#[test]
+fn test_parse_line_comment_spacey() {
+    assert_eq!(parse_line("  # don't run -a script  "),
+               Line::Comment);
+}
+
+#[test]
+fn test_parse_line_hash_name() {
+    assert_eq!(parse_line("hash#: this is valid"),
+               Line::Plugin("hash#", "this is valid"));
+}
+
+#[test]
+fn test_parse_line_err_no_name() {
+    assert_eq!(parse_line(": cat /dev/null"),
+               Line::SyntaxError("Plugin has no name"));
+}
+
+#[test]
+fn test_parse_line_err_no_name_spacey() {
+    assert_eq!(parse_line("  :  cat /dev/null   "),
+               Line::SyntaxError("Plugin has no name"));
+}
+
+#[test]
+fn test_parse_line_no_cmd() {
+    assert_eq!(parse_line("nothing:"),
+               Line::SyntaxError("Plugin has no command"));
+}
+
+#[test]
+fn test_parse_line_no_cmd_spacey() {
+    assert_eq!(parse_line("   nothing:  "),
+               Line::SyntaxError("Plugin has no command"));
 }
