094ddf98984a6e73378f411a8f95c276b8bd673b
Simon Bernier St-Pierre <sbernierstpierre@gmail.com>
2015-03-16 22:36:39 -0400

    Client waits before sending messages and listen forever.

--- examples/client.py
+++ examples/client.py
@@ -1,11 +1,14 @@
 import socket
 import sys
+import time
 
 so = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 so.connect(sys.argv[1])
-
 file = so.makefile('rw')
+
+time.sleep(0.1)
 file.write('HELLO\n')
 file.flush()
 
-print(file.readline())
+while True:
+    print(file.readline())
